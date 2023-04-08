28735e1e1f958366f8b989e433f69e77813d9198
Ami-hub <tnhh952@gmail.com>
2023-04-08 23:42:46 +0300

    add a host to listen

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { config } from "dotenv";
-import { cleanEnv, str, email, json } from "envalid";
+import { cleanEnv, str, port } from "envalid";
 
 config();
 
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({ choices: ["development", "test", "production", "staging"] }),
+  PORT: port(),
 });
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { env } from "./env";
 
 const main = async () => {
   const app = express();
-  const port = env.isDev ? 3000 : 80;
+  const port = env.PORT;
 
   app.get("/", (req, res) => {
     res.send({ message: "Hello World!" });
   });
 
-  app.listen(port, () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log(`Server is listening on port ${port}`);
   });
 };
