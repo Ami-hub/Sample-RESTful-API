dd183158d5e62308ffc01d038991dee1d2cf9ffe
Ami-hub <tnhh952@gmail.com>
2023-04-09 00:26:54 +0300

    remove render.yaml

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { env } from "./env";
 
 const main = async () => {
   const app = express();
-  const port = env.PORT;
 
   app.get("/", (req, res) => {
     res.send({ message: "Hello World!" });
   });
 
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is listening on port ${port}`);
+  app.listen(env.PORT, "0.0.0.0", () => {
+    console.log(`Server is listening on port ${env.PORT}`);
   });
 };
 
