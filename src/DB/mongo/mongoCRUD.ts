829445a8d9725c187e04579f8c65ad3dda3c7241
Ami-hub <tnhh952@gmail.com>
2023-04-26 12:53:29 +0300

    feat: add `readByField` + refactor `CRUD`

--- src/DB/mongo/mongoCRUD.ts
+++ src/DB/mongo/mongoCRUD.ts
@@ -3,51 +3,53 @@ import { EntitiesMap, IdType, IdKey } from "../../types/general";
 import { getDbInstance } from "./init";
 import { CRUD } from "../CRUD";
 
-// TODO: get rid of the casts to any!
-// if you have a better idea, please let me know...
-
 export const getMongoCRUD = <T extends keyof EntitiesMap>(
   collectionName: T
 ): CRUD<EntitiesMap[T]> => {
   const db: Db = getDbInstance();
-  const collection = db.collection<EntitiesMap[T]>(collectionName);
+  const collection = db.collection(collectionName);
 
   const readAll = async () => {
-    const res = await collection.find().toArray();
-    return res as Array<EntitiesMap[T]>; // TODO: get rid of this cast
+    const res = await collection.find<EntitiesMap[T]>({}).toArray();
+    return res;
   };
 
   const readById = async (id: IdType) => {
-    const result = await collection.findOne(id as any); // TODO: get rid of this cast
-    return result as EntitiesMap[T]; // TODO: get rid of this cast
+    const result = await collection.findOne<EntitiesMap[T]>(id);
+    return result;
+  };
+
+  const readByField = async <K extends keyof EntitiesMap[T]>(
+    field: K,
+    value: EntitiesMap[T][K]
+  ) => {
+    const result = await collection.findOne<EntitiesMap[T]>({ [field]: value });
+    return result;
   };
 
   const create = async (data: Omit<EntitiesMap[T], IdKey>) => {
-    const result = await collection.insertOne(data as any); // TODO: get rid of this cast
-    return result.insertedId as IdType;
+    const result = await collection.insertOne(data);
+    return result.acknowledged ? result.insertedId : null;
   };
 
   const update = async (
     id: IdType,
     data: Partial<Omit<EntitiesMap[T], IdKey>>
   ) => {
-    await collection.updateOne({ _id: id as any }, { $set: data as any }); // TODO: get rid of this cast
+    await collection.updateOne({ _id: id }, { $set: data });
     return await readById(id);
   };
 
   const deleteOne = async (id: IdType) => {
     const toDelete = await readById(id);
-    const res = await collection.deleteOne({ _id: id as any }); // TODO: get rid of this cast
-    res.acknowledged
-      ? console.log(`Deleted ${res.deletedCount} document(s).`)
-      : console.log("Delete failed.");
-
+    const res = await collection.deleteOne({ _id: id });
     return res.acknowledged ? toDelete : null;
   };
 
   return {
     readAll,
     readById,
+    readByField,
     create,
     update,
     delete: deleteOne,
