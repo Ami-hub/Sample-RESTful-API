ceb19d8e72c269ec25cf237ec8d87851b5e7b59b
Ami-hub <tnhh952@gmail.com>
2023-05-02 01:10:22 +0300

    refactor: rename `deferToErrorHandler`

--- src/routing/routes/errorHandler.ts
+++ src/routing/routes/errorHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-export const deferToErrorHandler = (
+export const deferToErrorMiddleware = (
   Fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
 ) => {
   return async (req: Request, res: Response, next: NextFunction) => {
