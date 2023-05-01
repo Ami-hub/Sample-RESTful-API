6c29d69042a11113ba9e705ce71f264154cbe75f
Ami-hub <tnhh952@gmail.com>
2023-05-02 01:41:09 +0300

    feat: add functions for initilazing the app

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,15 @@
 import express from "express";
 import { env } from "./env";
-import { getAccountRouter } from "./routing/routers/accountRouter";
 import { getDalManager } from "./DB/dalManager";
-import {
-  accountCollectionName,
-  mongoImplementationName,
-} from "./types/general";
-import { errorHandler } from "./routing/routes/errorHandler";
+import { mongoImplementationName } from "./types/general";
+import { initilizeApp, runApp } from "./setup/initSetUp";
 
 const main = async () => {
   const app = express();
 
-  const dalManager = await getDalManager(mongoImplementationName).connect();
-  const entityDalGetter = dalManager.getEntityDalByName;
+  await initilizeApp(app, mongoImplementationName);
 
-  app.use(express.json());
-
-  app.use(`/${accountCollectionName}`, getAccountRouter(entityDalGetter));
-
-  app.use(errorHandler);
-
-  app.get("/", (_req, res) => {
-    res.send({
-      Response: "Hello World",
-    });
-  });
-
-  app.listen(env.PORT, "0.0.0.0", () => {
-    console.log(`Server is listening at http://localhost:${env.PORT}`);
-  });
+  runApp(app);
 };
 
 main();
---
+++ src/setup/initSetUp.ts
@@ -0,0 +1,42 @@
+import { getAccountRouter } from "../routing/routers/accountRouter";
+import { errorHandler } from "../routing/routes/errorHandler";
+import { ImplementationNames, accountCollectionName } from "../types/general";
+import express, { Express } from "express";
+import { env } from "../env";
+import { getDalManager } from "../DB/dalManager";
+
+export const initilizeApp = async (
+  app: Express,
+  implementationName: ImplementationNames
+) => {
+  app.use(express.json());
+
+  await initilizeEntetiesRouters(app, implementationName);
+
+  app.use("/", (_req, res) => {
+    res.send({
+      Response: "Hello World",
+    });
+  });
+
+  app.use(errorHandler);
+
+  return app;
+};
+
+const initilizeEntetiesRouters = async (
+  app: Express,
+  implementationName: ImplementationNames
+) => {
+  const dalManager = await getDalManager(implementationName).connect();
+  const entityDalGetter = dalManager.getEntityDalByName;
+
+  app.use(`/${accountCollectionName}`, getAccountRouter(entityDalGetter));
+};
+
+export const runApp = async (app: Express) => {
+  app.listen(env.PORT, "0.0.0.0", () => {
+    console.log(`Server is up and listening on port ${env.PORT}`);
+  });
+  return app;
+};
