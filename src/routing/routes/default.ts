70402f6e7ac86dae153cee6e45309b8ab3c32abd
Ami Cahalon <tnhh952@gmail.com>
2023-05-08 22:06:37 +0300

    refactor: delete `iconRoutes`

--- src/routing/routes/default.ts
+++ src/routing/routes/default.ts
@@ -9,13 +9,3 @@ export const helloRoutes = (_req: Request, res: Response) => {
 export const defaultRoutes = (_req: Request, res: Response) => {
   res.status(StatusCodes.NOT_FOUND).json({ message: "Not Found" });
 };
-
-const iconPath =
-  "https://cdn.icon-icons.com/icons2/3962/PNG/512/globe_global_web_internet_international_earth_eco_icon_252571.png";
-
-export const iconRoutes = (req: Request, res: Response) => {
-  http.get(iconPath, (response) => {
-    res.set("Content-Type", "image/x-icon");
-    response.pipe(res);
-  });
-};
