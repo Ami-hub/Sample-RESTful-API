307fda8a76c96015763124dec8b626be100bf159
Ami Cahalon <tnhh952@gmail.com>
2023-05-14 03:20:14 +0300

    feat: create `getDefaultsRoutes` function

--- src/routing/routes/default.ts
+++ src/routing/routes/default.ts
@@ -1,11 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { createStatusError } from "../../errorHandling/statusError";
 
-export const helloRoutes = (_req: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({ message: "Hello World" });
-};
+export const getDefaultsRoutes = async () => {
+  const notFoundRoutes = (_req: Request, res: Response) => {
+    throw createStatusError(
+      `no such resource, please refer to the API documentation to see the available routes`,
+      StatusCodes.NOT_FOUND
+    );
+  };
 
-export const defaultRoutes = (_req: Request, res: Response) => {
-  throw createStatusError("no such resource", StatusCodes.NOT_FOUND);
+  const welcomeRoutes = (_req: Request, res: Response) => {
+    res.status(StatusCodes.OK).json({ message: "Welcome to the API" });
+  };
+
+  return {
+    notFoundRoutes,
+    welcomeRoutes,
+  };
 };
