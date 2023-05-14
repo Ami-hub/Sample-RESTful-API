342e171a3c2a83f5cf93d47f9955ea7d97c4ef33
Ami Cahalon <tnhh952@gmail.com>
2023-05-14 03:23:03 +0300

    feat: set retry optional

--- src/DB/mongo/init.ts
+++ src/DB/mongo/init.ts
@@ -3,6 +3,10 @@ import { env } from "../../env";
 import { EntitiesMap } from "../../types/general";
 import { logger } from "../../logging/logger";
 
+/**
+ * The client that connects to the DB
+ * @NOTE This project uses only one client for the DB!
+ */
 const client = new MongoClient(env.MONGODB_URI);
 
 /**
@@ -11,16 +15,24 @@ const client = new MongoClient(env.MONGODB_URI);
 const reconnectingIntervalMs = 15000;
 /**
  * Connects to the DB
+ * @param retry if true, will try to reconnect to the DB if the connection is failed
  * @NOTE if you have already connected to the DB, this function will do nothing
  */
-export const connectToDB = async () => {
+export const connectToDB = async (retry: boolean = false) => {
+  logger.verbose(`Trying to connect to '${env.MAIN_DB_NAME}' DB`);
   try {
     await client.connect();
     logger.info(`Connected to '${env.MAIN_DB_NAME}' DB`);
   } catch (error) {
-    logger.error(`Failed to connect to '${env.MAIN_DB_NAME}' DB`);
-    logger.error(`reconnecting in ${reconnectingIntervalMs / 1000} seconds...`);
-    setTimeout(connectToDB, reconnectingIntervalMs);
+    logger.error(`Failed to connect to '${env.MAIN_DB_NAME}' DB ${
+      retry ? `retrying in ${reconnectingIntervalMs / 1000} seconds...` : ""
+    }
+    `);
+    if (retry) {
+      setTimeout(() => {
+        logger.verbose(`try to reconnect to '${env.MAIN_DB_NAME}' DB...`);
+      }, reconnectingIntervalMs);
+    }
   }
 };
 
