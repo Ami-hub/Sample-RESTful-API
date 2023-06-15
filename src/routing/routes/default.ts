7009f5460d6a29734cfd20ef2078b0b7fd56c4aa
Ami Cahalon <tnhh952@gmail.com>
2023-06-15 18:58:50 +0300

    feat: remove favicon.ico handling support

--- src/routing/routes/default.ts
+++ src/routing/routes/default.ts
@@ -1,11 +1,6 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { createStatusError } from "../../errorHandling/statusError";
-
-/**
- * The standard favicon path
- */
-const faviconPath = "/favicon.ico";
 
 export const getDefaultsRoutes = async () => {
   const notFoundRoutes = (_req: Request, res: Response) => {
@@ -16,17 +11,11 @@ export const getDefaultsRoutes = async () => {
   };
 
   const welcomeRoutes = (_req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ message: "Welcome to the API" });
-  };
-
-  const faviconHandler = (req: Request, res: Response, next: NextFunction) => {
-    if (req.url === faviconPath) res.status(204).end();
-    else next();
+    res.status(StatusCodes.OK).json({ message: "welcome to the API" });
   };
 
   return {
     notFoundRoutes,
     welcomeRoutes,
-    faviconHandler,
   };
 };
