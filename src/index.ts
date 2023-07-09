603b62d7d6ad16e1856f19fdc5ddc5336bb557bb
Ami Cahalon <tnhh952@gmail.com>
2023-07-10 00:38:50 +0300

    refactor: move Application type to a new file

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
-import Fastify from "fastify";
-import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
 import { initializeApp, startListen } from "./setup/initSetUp";
-import { fastifyWinstonLogger, logger } from "./logging/logger";
+import { logger } from "./logging/logger";
 import { env } from "./setup/env";
 import { getDbConnector } from "./DB/databaseConnector";
-import { getCRUD } from "./DB/CRUD";
-
-/**
- * The main application instance
- * @see https://www.fastify.io/docs/latest/TypeScript/
- */
-const app = Fastify({
-  maxRequestsPerSocket: 1000,
-  logger: fastifyWinstonLogger,
-}).withTypeProvider<JsonSchemaToTsProvider>();
-
-/**
- * Fastify instance including the type provider
- */
-export type Application = typeof app;
+import { Application, getApplicationInstance } from "./application";
 
 /**
  * The first function to run when the application starts
  */
 const start = async () => {
+  const app: Application = getApplicationInstance();
+
   logger.info(`Starting in ${env.NODE_ENV} mode...`);
 
   const dbConnector = await getDbConnector();
