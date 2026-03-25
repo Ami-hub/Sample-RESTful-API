c071dab574718cb24ffa84e7b179f2598de50e70
Amitay Cahalon <tnhh952@gmail.com>
2026-03-26 01:35:54 +0200

    chore: update logger

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -6,24 +6,27 @@ import pinoPretty from "pino-pretty";
 import { env } from "../setup/env";
 
 const createPinoLogger = (level: LevelWithSilent) => {
+  const pinoOptions = {
+    level,
+    formatters: {
+      log: (object: Record<string, unknown>) => ({
+        ...object,
+        userId: requestContext.get("userId"),
+      }),
+    },
+    enabled: env.ENABLE_LOGGING,
+  };
+  
+  if (env.isProduction) {
+    return pino(pinoOptions);
+  }
+
   const stream = pinoPretty({
     colorize: true,
     translateTime: "yyyy-mm-dd HH:MM:ss.l o",
   });
 
-  return pino(
-    {
-      level,
-      formatters: {
-        log: (object) => ({
-          ...object,
-          userId: requestContext.get("userId"),
-        }),
-      },
-      enabled: env.ENABLE_LOGGING,
-    },
-    stream
-  );
+  return pino(pinoOptions, stream);
 };
 
 export const logger: FastifyBaseLogger = createPinoLogger(env.LOG_LEVEL);
