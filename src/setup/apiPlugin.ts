7d8ad8411e79572a053b76b1b4851b2222675673
Amitay Cahalon <tnhh952@gmail.com>
2026-03-26 01:36:19 +0200

    feat: add health routes

--- src/setup/apiPlugin.ts
+++ src/setup/apiPlugin.ts
@@ -4,10 +4,40 @@ import { StatusCodes } from "http-status-codes";
 import { logger } from "../logging/logger";
 import { setApiVersion1 } from "../routes/v1/apiV1Plugin";
 import { Application } from "../application";
+import { getDbConnector } from "../DB/databaseConnector";
 
 const welcomeRoute = async (_request: FastifyRequest, reply: FastifyReply) => {
   reply.status(StatusCodes.OK).send({
     message: `Welcome to the API`,
+  });
+};
+
+const livenessRoute = async (
+  _request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  reply.status(StatusCodes.OK).send({
+    status: "alive",
+  });
+};
+
+const readinessRoute = async (
+  _request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const dbConnected = await getDbConnector().isConnected();
+
+  if (!dbConnected) {
+    reply.status(StatusCodes.SERVICE_UNAVAILABLE).send({
+      status: "not ready",
+      dbConnected,
+    });
+    return;
+  }
+
+  reply.status(StatusCodes.OK).send({
+    status: "ready",
+    dbConnected,
   });
 };
 
@@ -19,6 +49,8 @@ export const setApi = async (app: Application) => {
       await setApiVersion1(api);
 
       api.get(`/`, welcomeRoute);
+      api.get(`/liveness`, livenessRoute);
+      api.get(`/readiness`, readinessRoute);
     },
     { prefix: API_PREFIX }
   );
